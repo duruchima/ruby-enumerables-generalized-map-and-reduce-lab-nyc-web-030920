f7a40115f765291b76512c7407286578a091d6a8
Duncan Uruchima <duruchima1@gmail.com>
2020-03-03 20:41:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,10 +10,15 @@ def map(array)
 end
 
 def reduce(array, starting_point=nil)
-  i=0
-  while i<array.length do
-    yield (array[i])
-    i+=1
+  if starting_point
+    num1=starting_point
+    i=0
+  else num1=array[0]
+    i=1
   end
-
+  while i<array.length
+    num1=yield (num1, array[i])
+    i+=1
+end
+num1
 end
